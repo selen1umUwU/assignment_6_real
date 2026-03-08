aa9f42d03c6d9ec63c7b4c4fac45116be323bd1a
selen1umUwU <thonghoangminhit123@gmail.com>
2026-03-08 22:43:34 +0700

    update

---
+++ Name_set.py
@@ -0,0 +1,14 @@
+names = set()
+while True:
+    name = input("Enter a name: ")
+    if name == "":
+        break
+    
+    if name in names:
+        print("Existing name")
+    else:
+        print("New name")
+        names.add(name)
+
+for n in names:
+    print(n)
